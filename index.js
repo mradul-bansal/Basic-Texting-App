b19abeba9c8bb899e4e916934d596e035beca6a5
mradul-bansal <9922102073@mail.jiit.ac.in>
2024-08-03 22:04:55 +0530

    Basic Chat Structure

--- index.js
+++ index.js
@@ -17,15 +17,12 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/Whatsapp');
 }
 
-let chat1 = new Chat({
-    from: "neha",
-    to: "Priya",
-    message: "Send me your exam sheets",
-    created_at: new Date(),
-});
 
-chat1.save().then((res) => {
-    console.log(res);
+//Index Route
+app.get("/chats", async (req, res) => {
+    let chats = await Chat.find()
+    console.log(chats);
+    res.render("index.ejs", { chats });
 });
 
 app.get("/", (req, res) => {
