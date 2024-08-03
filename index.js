90af1349666f8d42cdfd29222f16ce037495f120
mradul-bansal <9922102073@mail.jiit.ac.in>
2024-08-03 22:49:39 +0530

    Updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Chat = require("./models/chat.js")
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.urlencoded({extend: true}));
 
 
 main().then(() => { 
@@ -27,6 +27,32 @@ app.get("/chats", async (req, res) => {
     console.log(chats);
     res.render("index.ejs", { chats });
 });
+
+//New Route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+
+//Create Route
+app.post("/chats", async (req, res) => {
+    let { from, to, message } =req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        message: message,
+        created_at: new Date()
+    });
+    newChat
+    .save()
+    .then((res) => {
+        console.log("Chat saved successfully");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    res.redirect("/chats");
+ });
 
 app.get("/", (req, res) => {
     res.send("Working")
